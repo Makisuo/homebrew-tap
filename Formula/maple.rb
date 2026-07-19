c511da40bb1408f39fed2e3ca5e9f64dcf4a6760
Makisuo <31933546+Makisuo@users.noreply.github.com>
2026-07-19 13:58:32 +0200

    Update Maple to v0.0.13 (#1)

--- Formula/maple.rb
+++ Formula/maple.rb
@@ -10,8 +10,8 @@ class Maple < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/Makisuo/maple/releases/download/v0.0.11/maple-v0.0.11-aarch64-apple-darwin.tar.gz"
-      sha256 "d3accb88502c91c64b4876766e34de448a1425ee869233ea46dd147a1e9f7377"
+      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.13/maple-v0.0.13-aarch64-apple-darwin.tar.gz"
+      sha256 "c2fefe8b610a32e82141c6c82a5715877459b6c036b3ea39cb79b81e2fda844e"
     end
 
     on_intel do
@@ -21,13 +21,13 @@ class Maple < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/Makisuo/maple/releases/download/v0.0.11/maple-v0.0.11-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "4598d9fd53894ccaf279a27088bfa7e7d25c4034653edbc6a42c81e8093e7e20"
+      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.13/maple-v0.0.13-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "07c8f5291223faf7f00a54074d44ee2741c05da8cecd08a8668fb2c7d3d35bb1"
     end
 
     on_intel do
-      url "https://github.com/Makisuo/maple/releases/download/v0.0.11/maple-v0.0.11-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "9decdd2c03cf02e7a9ee9722f4d5d17dd93a9d57a97c5fe9464030a36e8f336e"
+      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.13/maple-v0.0.13-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "57265ed3a28e13839167d3f16362245b7b6712b226323d70056fa0299043a556"
     end
   end
 
