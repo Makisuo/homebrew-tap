077a38793af8303c68c6055a4d05440198562179
Makisuo <minetoaster12@gmail.com>
2026-08-19 14:07:21 +0200

    maple 0.0.19

--- Formula/maple.rb
+++ Formula/maple.rb
@@ -10,8 +10,8 @@ class Maple < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.13/maple-v0.0.13-aarch64-apple-darwin.tar.gz"
-      sha256 "c2fefe8b610a32e82141c6c82a5715877459b6c036b3ea39cb79b81e2fda844e"
+      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.19/maple-v0.0.19-aarch64-apple-darwin.tar.gz"
+      sha256 "823ad037adfabd39c939b4221466b28d857c68f4bf39f5b63f7331d9c9ca3439"
     end
 
     on_intel do
@@ -21,13 +21,13 @@ class Maple < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.13/maple-v0.0.13-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "07c8f5291223faf7f00a54074d44ee2741c05da8cecd08a8668fb2c7d3d35bb1"
+      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.19/maple-v0.0.19-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "f1ccc30373e3551c0f6e1f72af7b25c5086a330dcc5cac7a147833f5285c405d"
     end
 
     on_intel do
-      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.13/maple-v0.0.13-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "57265ed3a28e13839167d3f16362245b7b6712b226323d70056fa0299043a556"
+      url "https://github.com/MapleTechLabs/maple/releases/download/v0.0.19/maple-v0.0.19-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "e56585ad90acfb606b7f72de7dd09521cb6495f88a3e378c9103cc7af3606c56"
     end
   end
 
